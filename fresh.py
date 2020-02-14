e3315833a01a6ef469f2beec08b9c52bd690a451
BenjieA <benjie.asare@gmail.com>
2020-02-14 13:46:32 +0000

    temp

--- fresh.py
+++ fresh.py
@@ -1,1 +1,8 @@
+from random import randint
+def randomlength():
+    ran = randint(0, 40)
+    for i in range (ran):
+        print(i)
+    return(ran)
 
+print('i printed out', randomlength(), 'values')
