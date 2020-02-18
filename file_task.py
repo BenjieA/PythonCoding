88386aeae9adaaed81f57d5fa0398df208a7a6e5
BenjieA <benjie.asare@gmail.com>
2020-02-18 21:05:34 +0000

    modulo challenge 5 and 7

---
+++ file_task.py
@@ -0,0 +1,13 @@
+# TOE: Write a program that will find all such numbers which are divisible by 7, but are not a multiple of 5, between 2000 and 3200 (both included). The numbers obtained should be returned in a comma-separated sequence on a single line.
+
+
+def multi_finder(maxvl, minval):
+    trutharray = []
+    for i in range(maxvl):
+        if i>minval and i%7 == 0 and i%5 != 0:
+            trutharray.append(i)
+
+    print(str(trutharray).strip('[]'))
+
+multi_finder(3200,2000)
+
