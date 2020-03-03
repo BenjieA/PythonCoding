d54635fc916d036256661fe910a03a0a9329344d
BenjieA <60783997+BenjieA@users.noreply.github.com>
2020-03-03 15:14:20 +0000

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,15 @@
+sudo apt udate
+sudo apt install git
+git config --global user.name 'BenjieA'
+git config --global user.email 'benjie.asare@gmail.com'
+
+sudo apt-get install python3
+sudo apt-get install python3-pip
+sudo apt-get install python3-venv
+python3 -m venv venv
+git clone https://github.com/BenjieA/PythonCoding
+nano scritp3.sh >> python3 fresh.py
+chmod +x script3.sh
+mv script3.sh ./LinuxUbuntu
+cd LinuxUbuntu
+./LinuxUbuntu/script3.sh
